999211f96eeda7758fb7ceea377c4559bce39ea4
Alejandro <100450996@alumnos.uc3m.es>
2022-10-11 12:23:46 +0200

    done

--- progargs.cpp
+++ progargs.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <cstring>
+#include <dirent.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+using namespace std;
+
+void errores(int argc, char *argv[]) {
+    if(argc != 3) {
+        cerr << "Wrong format:\nimage in_path out_path oper\noperation: copy, histo, mono, gauss";
+    }
+
+    if(strcmp(argv[2], "copy") != 0  || strcmp(argv[2], "histo") != 0 || strcmp(argv[2], "mono") != 0 || strcmp(argv[2], "gauss") !=0){
+        cerr << "Unexpected operation:"<<argv[2]<<"\nimage in_path out_path oper\noperation: copy, histo, mono, gauss";
+    }
+
+    DIR *dirin = opendir(argv[0]);
+    if(dirin == nullptr){
+        cerr << "Input path: "<<argv[0]<<"\nOutput path: "<<argv[1]<<"\nCannot open directory: "<<argv[0]<<"\nimage in_path out_path oper\noperation: copy, histo, mono, gauss";
+    }
+
+    DIR *dirout = opendir(argv[1]);
+    if(dirout == nullptr){
+        cerr << "Input path: "<<argv[0]<<"\nOutput path: "<<argv[1]<<"\nOutput directory"<<argv[1]<<"does not exist\nimage in_path out_path oper\noperation: copy, histo, mono, gauss";
+    }
+
+}
+
+void tiempo_ejecucion(float tload, float tstore, float top, DIR *dirin, DIR *dirout,char archivo, char op){
+    cout << "Input path: "<<dirin<<"\nOutput path: "<<dirout<<"File: "<<archivo<<" (time: "<<tload+top+tstore<<")\nLoad time: "<<tload;
+    cout <<op<<" time: "<<top<<;
+
+
+    Load time: 3442
+    Gauss time: 38909
+    Store time: 1642
+    File: "indir/169012.bmp"(time: 37209)
+    Load time: 2602
 }
