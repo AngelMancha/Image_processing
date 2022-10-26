2435011209f8c927e0969d8c729dc44945470751
AngelMancha <100451302@alumnos.uc3m.es>
2022-10-26 20:50:20 +0200

    common.h

---
+++ common/common.h
@@ -0,0 +1,12 @@
+//
+// Created by xboxa on 26/10/2022.
+//
+
+#ifndef ARCOS_PROYECTO1_COMMON_H
+#define ARCOS_PROYECTO1_COMMON_H
+#include <iostream>
+#include <filesystem>
+namespace common {
+    const char * parametros_entrada()
+}
+#endif //ARCOS_PROYECTO1_COMMON_H
