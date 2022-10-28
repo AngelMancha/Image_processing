188cda5a385953f1317662ea00146fea88a2d309
Alejandro <100450996@alumnos.uc3m.es>
2022-10-28 13:21:33 +0200

    ...

--- aos/imgaos.cpp
+++ aos/imgaos.cpp
@@ -10,8 +10,6 @@ int main(int argc, char *argv[]) {
     std::vector<std::filesystem::path> archivosBMP;
     errores(argc, argv[1],argv[2],argv[3]);
     archivosBMP = getImgPaths(argv[1]);
-
-    //ahora ejecutar funcion, seguramente tenga que comprobar algo de errores
     Image::funcion(archivosBMP, static_cast<std::string>(argv[2]), static_cast<std::string>(argv[3]));
 
 }
--- soa/imgsoa.cpp
+++ soa/imgsoa.cpp
@@ -8,12 +8,9 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-    //if toda las cosas de errores de si histo y pollas
     std::vector<std::filesystem::path> archivosBMP;
     errores(argc, argv[1],argv[2],argv[3]);
     archivosBMP = getImgPaths(argv[1]);
-
-    //ahora ejecutar funcion, seguramente tenga que comprobar algo de errores
     ImageSoa::funcion(archivosBMP, static_cast<std::string>(argv[2]), static_cast<std::string>(argv[3]));
 
 }
