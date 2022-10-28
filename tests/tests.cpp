e6e8d5986af723fc331892a24f936733174056d6
Alejandro <100450996@alumnos.uc3m.es>
2022-10-28 13:19:14 +0200

    gtests

---
+++ tests/tests.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "../soa/Image_soa.h"
+#include "../aos/Image_aos.h"
+#include "../common/common.h"
+
+TEST(Google_tests, test1) {
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
+
+TEST(Google_tests, test2) {
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
